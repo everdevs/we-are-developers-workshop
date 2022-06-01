3f2527769e2478a6b088e5158decd7cd2a9d4abe
roman-zabeida <roman.zabeida@evernest.com>
2022-06-01 23:22:34 +0200

    chore: graphql setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
